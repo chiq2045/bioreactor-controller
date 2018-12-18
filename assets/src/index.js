90e8e402b23c6f8759151b7f945b09d673d37561
BioTech <charles.oroko@outlook.com>
2018-12-17 22:49:56 -0800

    Adds the initial reactjs pages

---
+++ assets/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <div>
+      Simple Sails-React starter
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
