ab5962f07d0df2f58daf7ef753ea2d5d17e9fc2f
BioTech <charles.oroko@outlook.com>
2018-12-18 16:55:16 -0800

    Changes names from index to main

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    entry: './assets/src/index.jsx'
+    entry: './assets/src/main.jsx'
   },
   output: {
     path: __dirname + '/.tmp/public',
@@ -24,7 +24,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'assets/src/index.html'
+      template: 'assets/src/main.html'
     })
   ]
 };
