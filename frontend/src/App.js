83c4fc5e6c16a78bfd1d9832e4d637c7d0bd6342
BioTech <charles.oroko@outlook.com>
2018-12-19 16:08:33 -0800

    First step toward a page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
+// import { Header } from './components/header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section id="app">
+        <header>Header</header>
+        <nav>Nav</nav>
+        <main>main</main>
+        <footer>footer</footer>
+      </section>
     );
   }
 }
