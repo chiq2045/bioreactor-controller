8cfbd66c5597ec918d5eecb75bf264ffa0040cf7
BioTech <charles.oroko@outlook.com>
2019-01-01 17:24:41 -0800

    Adds react-router-dom for single page app. Adds some pages as components.

---
+++ assets/js/components/runs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Runs extends Component {
+  render() {
+    return (
+      <div>runs</div>
+    )
+  }
+}
+
+export default Runs
--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+import Dashboard from './components/dashboard.jsx'
+import Data from './components/data.jsx'
+import Profile from './components/profile.jsx'
+import Runs from './components/runs.jsx'
 
 const { render } = ReactDOM
 
-class HelloWorld extends React.Component {
+class Main extends React.Component {
   render() {
     return (
-      <div>Hello World</div>
+      setupRoutes()
     )
   }
 }
 
-render (
-  <HelloWorld />, document.getElementById("react-container")
-)
+function setupRoutes() {
+  return (
+    <HashRouter>
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className='header'>
+          <button className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent'><NavLink to='/'>Home</NavLink></button>
+          <li><NavLink to='/data'>Data</NavLink></li>
+          <li><NavLink to='/runs'>Runs</NavLink></li>
+          <li><NavLink to='/profile'>Profile</NavLink></li>
+        </ul>
+        <div className='content'>
+          <Route exact path='/' component={Dashboard}/>
+          <Route path='/data' component={Data}/>
+          <Route path='/runs' component={Runs}/>
+          <Route path='/profile' component={Profile}/>
+        </div>
+      </div>
+    </HashRouter>
+  )
+}
+
+render (<Main />, document.getElementById('react-container'))
