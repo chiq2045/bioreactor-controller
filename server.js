3db0e2c7410a093d32a50fb661ed269d70ecd17a
Chiq2045 <charles.oroko@outlook.com>
2019-05-04 07:36:58 +0000

    create a basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  ctx.body = 'Welcome to the bioreactor controller app';
+});
+
+app.listen(3000);
+
+module.exports = app;
+
