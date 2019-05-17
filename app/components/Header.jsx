2a2c7e8e599155a1d46c851ca1cd34f639a18266
Chiq2045 <charles.oroko@outlook.com>
2019-05-17 03:03:13 +0000

    update bioreactor source code

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Welcome to the Onum App</h1>
+        <h1>Welcome to Bioreactor Controller app</h1>
       </header>
     );
   }
